b739cb83880924c41e0221b4119f97cfb865dae0
Pedro Cardozo <p-cardozo@hotmail.com>
2022-06-03 08:08:20 -0300

    update: new folder view included

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "./App";
+import App from "./View/App";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GeistProvider, CssBaseline } from "@geist-ui/core";
